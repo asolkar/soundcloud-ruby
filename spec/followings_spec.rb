96fa4a0c92336af5836ee98885e95f66cc988a20
Mahesh Asolkar <mahesh@mahesha.com>
2013-11-15 14:48:46 -0800

    Added test for id that I don't follow

--- spec/followings_spec.rb
+++ spec/followings_spec.rb
@@ -26,6 +26,22 @@ describe SoundCloud do
         expect(resp[0].kind).to eq('user')
       end
     end
+    #
+    # If you use an id that I don't follow, expect a response error.
+    # 37626 is a random user ID. If you happen to follow it, use some
+    # other id
+    #
+    describe "#get /me/followings/37626" do
+      it "returns not found" do
+        # pp resp.inspect
+        expect do
+          subject.send(:get, "/me/followings/37626")
+        end.to raise_error(SoundCloud::ResponseError)
+      end
+    end
+    #
+    # If you use an id that I do follow, expect an OK response with user object
+    #
     describe "#get /me/followings/#{ENV['TEST_ID']}" do
       it "returns authenticated user following for id #{ENV['TEST_ID']}" do
         resp = subject.send(:get, "/me/followings/#{ENV['TEST_ID']}")
